49c3fd86bb3a52aec2f21af1ff48631374aa94fc
embeddedchief <samuelsetemijp@gmail.com>
2026-03-18 10:39:09 +0100

    first program

---
+++ learning.dart
@@ -0,0 +1,8 @@
+void main(){
+  var firstName =  "Samuel"; //decalring first name and assigning value to it
+  var lastName = "Oluwasetemi"; //declaring last name and assigning value to it 
+
+  //Printing the full name using string interpoliation
+  print("My name is $firstName $lastName");
+
+}
