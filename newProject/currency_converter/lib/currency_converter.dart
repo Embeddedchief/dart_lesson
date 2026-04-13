f383b5a1aa523aa6b7411a17deca0688f1ea5088
embeddedchief <samuelsetemijp@gmail.com>
2026-04-14 00:16:46 +0100

    #DAy26

--- newProject/currency_converter/lib/currency_converter.dart
+++ newProject/currency_converter/lib/currency_converter.dart
@@ -6,11 +6,40 @@ class CurrencyConverter extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
+      backgroundColor: Color.fromRGBO(0, 0, 0, 0),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
-          children: [Text("0")],
+          children: [
+            Text(
+              "0",
+              style: TextStyle(
+                fontSize: 35,
+                fontWeight: FontWeight.bold,
+                color: Color.fromARGB(255, 255, 255, 255),
+              ),
+            ),
+            TextField(
+              style: TextStyle(color: Colors.black),
+              decoration: InputDecoration(
+                hintText: "Please enter the ammount in USD",
+                hintStyle: TextStyle(color: Colors.black),
+                suffixIcon: Icon(Icons.monetization_on_outlined),
+                suffixIconColor: Colors.black,
+                filled: true,
+                fillColor: Color.fromARGB(255, 255, 255, 255),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.tealAccent,
+                    width: 2.0,
+                    style: BorderStyle.solid,
+                    strokeAlign: BorderSide.strokeAlignCenter,
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
--- newProject/currency_converter/lib/main.dart
+++ newProject/currency_converter/lib/main.dart
@@ -2,11 +2,11 @@ import 'package:currency_converter/currency_converter.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(myApp());
+  runApp(MyApp());
 }
 
-class myApp extends StatelessWidget {
-  const myApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
