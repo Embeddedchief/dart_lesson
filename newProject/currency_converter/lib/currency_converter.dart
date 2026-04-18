09cd74c24db9c50c3d85ef9e0ac9158865d33a9c
embeddedchief <samuelsetemijp@gmail.com>
2026-04-19 00:11:54 +0100

    #Day31

--- newProject/currency_converter/lib/currency_converter.dart
+++ newProject/currency_converter/lib/currency_converter.dart
@@ -5,12 +5,40 @@ import 'package:flutter/material.dart';
 // Store the value and
 // Dislay the value
 
-class CurrencyConverter extends StatelessWidget {
+class CurrencyConverter extends StatefulWidget {
   const CurrencyConverter({super.key});
 
   @override
+  State<CurrencyConverter> createState() => _CurrencyConverterState();
+}
+
+class _CurrencyConverterState extends State<CurrencyConverter> {
+  final TextEditingController textEditingController = TextEditingController();
+  double result = 0;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    textEditingController.dispose();
+    super.dispose();
+  }
+
+  void _calculateConversion() {
+    setState(() {
+      if (textEditingController.text.isNotEmpty) {
+        result = double.parse(textEditingController.text) * 1032;
+      } else {
+        result = 0;
+      }
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    //double result = 0;
     return Scaffold(
       backgroundColor: Color.fromRGBO(222, 223, 216, 1),
       appBar: AppBar(
@@ -28,8 +56,8 @@ class CurrencyConverter extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Text(
-              "0",
-              style: TextStyle(
+              "N ${result.toStringAsFixed(2)}",
+              style: const TextStyle(
                 fontSize: 35,
                 fontWeight: FontWeight.bold,
                 color: Color.fromARGB(255, 163, 177, 170),
@@ -39,6 +67,7 @@ class CurrencyConverter extends StatelessWidget {
               padding: EdgeInsets.all(30.0),
 
               child: TextField(
+                controller: textEditingController,
                 style: TextStyle(color: Colors.black),
                 decoration: InputDecoration(
                   hintText: "Please enter the ammount in USD",
@@ -61,15 +90,20 @@ class CurrencyConverter extends StatelessWidget {
               ),
             ),
             TextButton(
-              onPressed: () {
-                debugPrint("You have clicked the button");
-              },
+              onPressed: _calculateConversion,
               style: TextButton.styleFrom(
                 backgroundColor: const Color.fromARGB(255, 41, 78, 1),
                 foregroundColor: const Color.fromARGB(255, 255, 255, 255),
                 fixedSize: Size(350, 50),
               ),
               child: const Text("Convert", style: TextStyle(fontSize: 20)),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Text(
+                "Note: We use the rate of N1032 per Canadian Dollar",
+                style: TextStyle(color: Colors.black, fontSize: 10),
+              ),
             ),
           ],
         ),
